c09acd341938daad82de63f5500fdccb737d70ed
@box406 <box406@gmail.com>
2013-12-06 22:20:38 +0900

    各トラックに対しての仮データをバインディング

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -9,16 +9,47 @@ app.controller("loginController", function($scope, $location){
 
 app.controller("tracController", function($scope, $http, $resource){
 
-  $scope.projectName = "";
-  $scope.showBoxTracs = [];
-  $scope.tracReports = [];
-
   var accuracysList = $resource("getAccuracy.php");
   $scope.accuracys = accuracysList.get();
 
   var parsonList = $resource("getParsonList.php");
   $scope.parsonList = parsonList.get();
+ 
+  $scope.projectName = "";
+  $scope.showBoxTracs = [];
+  $scope.nonCostomersBoxTracs = [{
+    "trac_id": Math.floor(Math.random() * 1000000),
+    "charge_parson": $scope.parsonList[1],
+    "trac_name": "test",
+    "accuracys": $scope.accuracys,
+    "accuracyDefault": "A"
+  }];
+  $scope.salesBoxTracs = [{
+    "trac_id": Math.floor(Math.random() * 1000000),
+    "charge_parson": $scope.parsonList[1],
+    "trac_name": "test",
+    "accuracys": $scope.accuracys,
+    "accuracyDefault": "A"
+  }];
+  $scope.orderBoxTracs = [{
+    "trac_id": Math.floor(Math.random() * 1000000),
+    "charge_parson": $scope.parsonList[1],
+    "trac_name": "test",
+    "accuracys": $scope.accuracys,
+    "accuracyDefault": "A"
+  }];
+  $scope.lostBoxTracs = [{
+    "trac_id": Math.floor(Math.random() * 1000000),
+    "charge_parson": $scope.parsonList[1],
+    "trac_name": "test",
+    "accuracys": $scope.accuracys,
+    "accuracyDefault": "A"
+  }];
+  $scope.tracReports = [];
+
   
+
+ 
   $scope.addSelectParsonClick = function(item) {
 
     $scope.showBoxTracs.push({

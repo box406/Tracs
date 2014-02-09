877208342264388ce4a4fd8cff0f09f880b0466e
@box406 <box406@gmail.com>
2014-02-10 08:05:08 +0900

    配列の持ち方を修正、tracIDをkeyにするやり方をやめて、0からの数字がkeyになるように修正

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -18,33 +18,24 @@ app.controller("tracController", function($scope, $rootScope, $http, $resource, 
   var tracList = $resource("http://dev.trac.com/tracs/");
   $scope.tracs = tracList.get();
 
+  var sortableEle;
+
   $scope.projectName = "";
   $scope.tracReports = [];
 
   $scope.addSelectParsonClick = function(item) {
 
-    // create tracId
-    trac_id = Math.floor(Math.random() * 1000000);
-
-    // add new trac
     new_trac = {
-        "trac_id": trac_id,
-        "charge_member": $scope.memberList[item],
-        "trac_name": $scope.projectName,
-        "accuracys": $scope.accuracys,
-        "accuracyDefault": "A",
-        "member_id": item,
-        "client_id": "1",
-        "accuracy_id": "1"
+      "trac_id": Math.floor(Math.random() * 1000000),
+      "charge_member": $scope.memberList[item],
+      "trac_name": $scope.projectName,
+      "accuracys": $scope.accuracys,
+      "accuracyDefault": "A",
+      "member_id": item,
+      "client_id": "1",
+      "accuracy_id": "1"
     };
-    tmp_trac = {};
-    tmp_trac[trac_id] = new_trac;
-    console.log(tmp_trac);
-    console.log($scope.tracs);
-    //$scope.tracs.showBox[trac_id] =  new_trac;
-    $scope.tracs.showBox.push(tmp_trac);
-    
-    $rootScope.hoge = "hogefuga";
+    $scope.tracs.showBox.push(new_trac);
 
     // add database
     adminTracService.registTrac(new_trac);
@@ -55,5 +46,76 @@ app.controller("tracController", function($scope, $rootScope, $http, $resource, 
   $scope.addTracReportClick = function() {
     $scope.tracReports.push({}); 
   }
-});
 
+  $scope.dragStart = function(e, ui) {
+    console.log("start : " + ui.item.index());  
+    $scope.start = ui.item.index();
+    $scope.startTracArea = $(this).attr("id");
+  }
+
+  $scope.dragReceive = function(e, ui) {
+    console.log("receive");
+    //console.log("strat : " + $scope.start);
+    //console.log("startTracArea : " + $scope.startTracArea);
+    //console.log("receive : " + ui.item.index());
+    //removeItem = $scope.tracs.showBox.splice($scope.start, 1);
+    //$scope.tracs.salesBox.splice(ui.item.index(), 0, removeItem[0]);
+    //console.log($scope.tracs);
+  }
+
+  $scope.dragUpdate = function(e, ui) {
+
+    if ($scope.startTracArea == $(this).attr("id")) {
+      //console.log("update");
+      //console.log($(this).attr("id"));
+    }
+    console.log("update");
+
+    //console.log("update : " + ui.item.index());
+    //console.log("update : " + $(this).attr("id"));
+    //console.log("-------");
+    //removeItem = $scope.tracs.showBox.splice($scope.start, 1);
+    //$scope.tracs.salesBox.splice(ui.item.index(), 0, removeItem[0]);
+    //console.log($scope.tracs);
+  }
+
+  $scope.dragStop = function(e, ui) {
+    //console.log("stop : " + ui.item.index());
+    //console.log($(this).attr("id"));
+  }
+
+
+  var TRAC_AREA_INFO = ["show-box", "non-costomers-box", "sales-box", "order-box", "lost-box"];
+  for (trac_area in TRAC_AREA_INFO) {
+    sortableEle = $('#' + TRAC_AREA_INFO[trac_area]).sortable({
+      connectWith: ".sortable-div", 
+      placeholder: "",
+      cursor: "move",
+      opacity: 0.5,
+      revert: true,
+      scroll: true,
+      receive: $scope.dragReceive,
+      start: $scope.dragStart,
+      update: $scope.dragUpdate,
+      stop: $scope.dragStop
+      //receive: function( event, ui ) {
+        //console.log($scope.tracs);
+        // move the trac area
+        //tracId = ui.item.attr("title");
+        //tracAreaId = $(this).attr("id");
+        //moveTracService.getMove({"tracArea":tracAreaId, "tracId":tracId});
+      //},
+      //start: function(event, ui) {
+        //console.log($scope.tracs);
+        //tracId = ui.item.attr("title");
+        //tracAreaId = $(this).attr("id");
+      //},
+      //update: function(event, ui) {
+        //console.log($scope.tracs);
+        // sort the trac area
+        //trac_area_name = $(this).attr("id")
+        //moveTracService.getUpdate($('#' + trac_area_name).sortable("toArray").join());
+      //}
+    });
+  }
+});

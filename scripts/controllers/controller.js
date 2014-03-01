6008184ac5cd2842d99a2712719f7ff266b6813d
@box406 <box406@gmail.com>
2014-03-01 16:51:34 +0900

    trac_areaの名前をスネークスタイルに統一、trac間の移動に合わせてbindしている変数の配列を変更するように変更した

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -22,6 +22,7 @@ app.controller("tracController", function($scope, $rootScope, $http, $resource, 
 
   $scope.projectName = "";
   $scope.tracReports = [];
+  $scope.receiveFlag = false;
 
   $scope.addSelectParsonClick = function(item) {
 
@@ -35,7 +36,7 @@ app.controller("tracController", function($scope, $rootScope, $http, $resource, 
       "client_id": "1",
       "accuracy_id": "1"
     };
-    $scope.tracs.showBox.push(new_trac);
+    $scope.tracs.show_box.push(new_trac);
 
     // add database
     adminTracService.registTrac(new_trac);
@@ -48,44 +49,35 @@ app.controller("tracController", function($scope, $rootScope, $http, $resource, 
   }
 
   $scope.dragStart = function(e, ui) {
-    console.log("start : " + ui.item.index());  
     $scope.start = ui.item.index();
     $scope.startTracArea = $(this).attr("id");
   }
 
   $scope.dragReceive = function(e, ui) {
-    console.log("receive");
-    //console.log("strat : " + $scope.start);
-    //console.log("startTracArea : " + $scope.startTracArea);
-    //console.log("receive : " + ui.item.index());
-    //removeItem = $scope.tracs.showBox.splice($scope.start, 1);
-    //$scope.tracs.salesBox.splice(ui.item.index(), 0, removeItem[0]);
-    //console.log($scope.tracs);
+    $scope.receiveFlag = true;
   }
 
   $scope.dragUpdate = function(e, ui) {
+    if ($scope.receiveFlag == true) {
+      $scope.receiveFlag = false;
 
-    if ($scope.startTracArea == $(this).attr("id")) {
-      //console.log("update");
-      //console.log($(this).attr("id"));
+
+      removeItem = $scope.tracs[$scope.startTracArea].splice($scope.start, 1);
+      $scope.tracs[$(this).attr("id")].splice(ui.item.index(), 0, removeItem[0]);
+
+      console.log("1:" + "start: " + $scope.startTracArea + ", update: " + $(this).attr("id"));
+      console.log($scope.tracs);
+    } else {
+
+      removeItem = $scope.tracs[$scope.startTracArea].splice($scope.start, 1);
+      $scope.tracs[$scope.startTracArea].splice(ui.item.index(), 0, removeItem[0]);
+
+      console.log("2:" + "start: " + $scope.startTracArea + ", update: " + $(this).attr("id"));
+      console.log($scope.tracs);
     }
-    console.log("update");
-
-    //console.log("update : " + ui.item.index());
-    //console.log("update : " + $(this).attr("id"));
-    //console.log("-------");
-    //removeItem = $scope.tracs.showBox.splice($scope.start, 1);
-    //$scope.tracs.salesBox.splice(ui.item.index(), 0, removeItem[0]);
-    //console.log($scope.tracs);
   }
 
-  $scope.dragStop = function(e, ui) {
-    //console.log("stop : " + ui.item.index());
-    //console.log($(this).attr("id"));
-  }
-
-
-  var TRAC_AREA_INFO = ["show-box", "non-costomers-box", "sales-box", "order-box", "lost-box"];
+  var TRAC_AREA_INFO = ["show_box", "non_costomers_box", "sales_box", "order_box", "lost_box"];
   for (trac_area in TRAC_AREA_INFO) {
     sortableEle = $('#' + TRAC_AREA_INFO[trac_area]).sortable({
       connectWith: ".sortable-div", 
@@ -96,26 +88,7 @@ app.controller("tracController", function($scope, $rootScope, $http, $resource, 
       scroll: true,
       receive: $scope.dragReceive,
       start: $scope.dragStart,
-      update: $scope.dragUpdate,
-      stop: $scope.dragStop
-      //receive: function( event, ui ) {
-        //console.log($scope.tracs);
-        // move the trac area
-        //tracId = ui.item.attr("title");
-        //tracAreaId = $(this).attr("id");
-        //moveTracService.getMove({"tracArea":tracAreaId, "tracId":tracId});
-      //},
-      //start: function(event, ui) {
-        //console.log($scope.tracs);
-        //tracId = ui.item.attr("title");
-        //tracAreaId = $(this).attr("id");
-      //},
-      //update: function(event, ui) {
-        //console.log($scope.tracs);
-        // sort the trac area
-        //trac_area_name = $(this).attr("id")
-        //moveTracService.getUpdate($('#' + trac_area_name).sortable("toArray").join());
-      //}
+      update: $scope.dragUpdate
     });
   }
 });
